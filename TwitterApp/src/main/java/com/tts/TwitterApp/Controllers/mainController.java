57fc4440acec7e8424b9366b04145991ac9c9e7c
hraczoraz1 <justinhraczo@gmail.com>
2020-12-16 18:36:44 -0500

    Initial Commit

---
+++ TwitterApp/src/main/java/com/tts/TwitterApp/Controllers/mainController.java
@@ -0,0 +1,13 @@
+package com.tts.TwitterApp.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mainController {
+	@GetMapping("/")
+    public String main(Model model) {
+		return "main";
+	}
+}
---
+++ TwitterApp/src/main/java/com/tts/TwitterApp/TwitterApp.java
@@ -0,0 +1,13 @@
+package com.tts.TwitterApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterApp.class, args);
+	}
+
+}
