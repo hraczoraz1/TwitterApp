d9cdc026f002ca4ff715143d1cc99cb993d3097e
hraczoraz1 <justinhraczo@gmail.com>
2020-12-18 16:54:35 -0500

    Add files via upload

--- TwitterApp/src/main/java/com/tts/TwitterApp/Controllers/mainController.java
+++ TwitterApp/src/main/java/com/tts/TwitterApp/Controllers/mainController.java
@@ -1,5 +1,8 @@
 package com.tts.TwitterApp.Controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.social.twitter.api.TwitterProfile;
@@ -7,21 +10,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.tts.TwitterApp.Service.NamexTweet;
+import com.tts.TwitterApp.Service.TweetService;
+
+import twitter4j.ResponseList;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
 @Controller
 public class mainController {
 	@Autowired
-	private Twitter twitter;
+	public TweetService tweetService;
 	
 	@GetMapping("/")
     public String main(Model model) {
+		List<String> t = tweetService.getLatestTweets();
+		System.out.println(t);
 		return "main";
 	}
 	
-	@GetMapping("/habuma")
-		public String main1(Model model) {
-			TwitterProfile profile = twitter.userOperations().getUserProfile("habuma");
-			model.addAttribute("habuma", profile);
-			return "main";
-		}
+	
 	
 }
